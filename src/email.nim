0a5388b9c242e721396d10d83a8bd16236597a1f
ThomasTJdev <ttjarloev@gmail.com>
2019-09-01 11:53:42 +0200

    Fix bug in email

--- src/email.nim
+++ src/email.nim
@@ -1,7 +1,6 @@
 # Copyright 2018 - Thomas T. Jarløv
 
-import asyncdispatch, smtp, strutils, os, asyncnet
-import logging
+import asyncdispatch, smtp, strutils, os, asyncnet, logging, parsecfg
 
 type
   SMTPDetails* = ref object
@@ -15,7 +14,17 @@ type
 var smtpDetails*: SMTPDetails
 new(smtpDetails)
 
-smtpDetails.address = ""
+let dict = loadConfig(getAppDir().replace("/src", "") / "config.cfg")
+
+# Set up SMTP
+smtpDetails.address   = dict.getSectionValue("SMTP", "SMTPAddress")
+smtpDetails.port      = dict.getSectionValue("SMTP", "SMTPPort")
+smtpDetails.fromMail  = dict.getSectionValue("SMTP", "SMTPFrom")
+smtpDetails.user      = dict.getSectionValue("SMTP", "SMTPUser")
+smtpDetails.password  = dict.getSectionValue("SMTP", "SMTPPassword")
+for i in split(dict.getSectionValue("SMTP", "SMTPMailTo"), ","):
+  smtpDetails.toMail.add(i)
+debug($smtpDetails[])
 
 proc sendMail*(subject, message: string) {.async.} =
   ## Send the email through smtp
@@ -23,7 +32,7 @@ proc sendMail*(subject, message: string) {.async.} =
   when defined(noemail):
     info("Email disabled - skipped mail")
 
-  if smtpDetails.address.len() == 0:
+  if smtpDetails.address == "":
     error("No SMTP address has been specified. Email was not send.")
     return
 
